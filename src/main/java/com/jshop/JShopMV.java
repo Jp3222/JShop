d5786103bd1b506051449a7b17304ea65ddc34e0
Jp3222 <juanpc.559@gmail.com>
2024-11-22 01:04:46 -0600

    migracion a mv

---
+++ src/main/java/com/jshop/JShopMV.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jshop;
+
+/**
+ *
+ * @author juan-campos
+ */
+public class JShopMV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
